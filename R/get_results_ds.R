9747d6a7d3710936d85c583190ec682bbf95fb6f
antonmalko <amalko@umd.edu>
2018-03-10 15:04:04 -0500

    Updated documentation

--- R/get_results_ds.R
+++ R/get_results_ds.R
@@ -8,6 +8,89 @@ get_results_ds <- function(file_name,
                           sprt = TRUE,
                           ...) {
 
+  #' Get results from a "DashedSentence" controller
+  #'
+  #' @param file_name character. Path to the file with Ibex results
+  #' @param elem_number Element number in IBEX. Only useful when a) You
+  #'   specified multiple controllers within one item and b) Some of those
+  #'   controllers have the same type. In this case you can specify which
+  #'   exactly of those duplicate controllers you need.
+  #' @param del_mode how to determine which columns have to be deleted. Can be one of:
+  #'          `auto` (decide based on data), `user` (only take into account
+  #'          user's requests), `mixed` (take into account user request and data).
+  #'          `auto` is used by default if "del_col" is not specified; if it is,
+  #'          `mixed` is used. See Details
+  #' @param col_names character vector with names for the columns
+  #' @param partial_names logical. If `TRUE`, the first seven columns will receive
+  #'                      default names (`timestamp`, `md5_hash`, `controller`,
+  #'                      `item_number`, `element_number`, `type`, `group`),
+  #'                      since these columns are the same in all (default)
+  #'                      Ibex controllers. So `col_names` will be taken to
+  #'                      specify names for columns starting from 8.
+  #'                      If `FALSE`, `col_names` will be taken as specifying names
+  #'                      for all columns
+  #' @param col_classes character vector with classes to be assumed for the columns
+  #' @param partial_classes logical. If `TRUE`, the first seven columns will receive
+  #'                      default classes:
+  #'
+  #'                      + timestamp, md5_hash, controller, type, group - `character`,
+  #'                      + item_number, element_number - `numeric`,
+  #'
+  #'                      So `col_classes` will be taken to specify classes for columns
+  #'                      starting from 8. If `FALSE`, `col_classes` will be taken
+  #'                      as specifying classes for all columns
+  #'  @param short_subj_ids logical. If `TRUE`, each subject will be assigned
+  #'  a short index. If `FALSE`, a combination of timestamp and browser md5 hash
+  #'  will be used to identify each subject.
+  #'  @param sprt logical. Set to `TRUE` if the data comes from "DashedSentence"
+  #'  controlled with `mode: "self-paced reading"` setting. Set to `FALSE` if
+  #'  it had `mode: "acceptability judgment` setting.
+  #'  @param ... further parameters passed to `read.csv`
+  #'
+  #'
+  #' @return data.frame
+  #' @export
+  #'
+  #' @details
+  #'
+  #' The first seven columns in IBEX results always are:
+  #' \enumerate{
+  #' \item Time the results are received
+  #' \item MD5 hash of participant's IP
+  #' \item Controller type
+  #' \item Item number
+  #' \item Element number
+  #' \item Type
+  #' \item Group
+  #'}
+  #' By default, the deletion mode is `auto`. It means that the function will
+  #' remove columns not containing useful information. This means:
+  #'
+  #' \itemize{
+  #' \item "md5 hash" column is always removed since it's not usually used in analyses
+  #' \item "controller" column is removed if there is just one controller in the data -
+  #'   it is assumed, the user will know what controller that is
+  #' \item "element number" column is removed if that column only contains zeros
+  #' \item "group" column is removed if it only contains `NULL` values
+  #' }
+  #'
+  #' The above applies to all controllers. In addition:
+  #'
+  #' \itemize{
+  #' \item for DashedSentence and DashedAccpetabilityJudgment column 11 ("line break")
+  #' is removed if it only contains `FALSE` values.
+  #' \item For DashedAcceptabilityJudgment only column 15 ("is.correct") is removed
+  #' if it contains only `NULL` valuess (if this is the case, it means that correct
+  #' answers were not specified in IBEX, so this column is of no use).
+  #' }
+  #'
+  #' In `user` deletion mode the data will not be taken into account at all, and
+  #' only the columns specified by user will be deleted.
+  #'
+  #' In `mixed` deletion mode the columns to delete are the union of the sets of
+  #' automatically deleted columns and user-specified columns.
+
+
   if (sprt){ # if the mode is "self-paced reading"
 
     if (is.null(col_names)){
